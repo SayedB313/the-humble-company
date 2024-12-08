c91ff9f70957d5e760d14580820f3a7c29c434c6
sayedb313 <sbw919@gmail.com>
2024-12-08 17:56:59 -0500

    UTC update

--- src/app/services/[service]/page.tsx
+++ src/app/services/[service]/page.tsx
@@ -1,16 +1,20 @@
 import Footer from '../../../components/ui/Footer';
+import CalEmbed from '../../../components/CalEmbed';
 
-// ... rest of the imports and component code ...
+export default function ServicePage() {
+  // Add any necessary state or logic here
+  const [isCalOpen, setIsCalOpen] = React.useState(false);
 
-return (
-  <div className="flex flex-col min-h-screen bg-white">
-    {/* ... existing code ... */}
-    
-    <Footer />
+  return (
+    <div className="flex flex-col min-h-screen bg-white">
+      {/* ... existing code ... */}
+      
+      <Footer />
 
-    <CalEmbed 
-      isOpen={isCalOpen}
-      onClose={() => setIsCalOpen(false)}
-    />
-  </div>
-); 
+      <CalEmbed 
+        isOpen={isCalOpen}
+        onClose={() => setIsCalOpen(false)}
+      />
+    </div>
+  );
+} 
